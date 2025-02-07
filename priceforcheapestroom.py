f7b0479745b9795e15f75e59bac553543cd7fdb8
Bharathkusuru <bharath.kusuru@nuware.com>
2025-02-07 16:01:37 +0530

    Add files via upload

---
+++ priceforcheapestroom.py
@@ -0,0 +1,14 @@
+import io
+def main():
+
+    my_path = "D:\abc"
+    csv_file = "cap_rooms.csv"
+    md_file = "cap_rooms.md"
+    csv_file_path = os.path.join(my_path,csv_file)
+    md_file_path = os.path.join(my_path, md_file)
+    open(md_file_path,'r',encoding='utf-8') as f:
+        md_content = f.read()
+    df = pd.read_csv(csv_file_path)
+    result = df.groupby('city')["price"].agg(cheapest = 'min', costlist ='max').reset_index()
+
+    print(result)
